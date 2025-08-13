7e502ffed693af3216117f0ce043a3993001c9bd
Luis <privatenumslower@gmail.com>
2025-08-12 23:55:32 -0500

    Updating NumOS files after recent edits

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -88,52 +88,6 @@ void print_hex(uint64_t value) {
     vga_writestring(buffer);
 }
 
-void show_gdt_info(void) {
-    uint16_t gdt_limit;
-    uint64_t gdt_base;
-    
-    // Get current GDT info using SGDT instruction
-    __asm__ volatile (
-        "sgdt %0"
-        : "=m" (gdt_limit), "=m" (gdt_base)
-        :
-        : "memory"
-    );
-    
-    vga_writestring("GDT Information:\n");
-    vga_writestring("  Base Address: ");
-    print_hex(gdt_base);
-    vga_writestring("\n  Limit: ");
-    print_hex(gdt_limit);
-    vga_writestring("\n  Entries: ");
-    
-    // Calculate number of entries
-    int entries = (gdt_limit + 1) / 8;
-    char num_str[10];
-    int i = 0;
-    int temp = entries;
-    
-    if (temp == 0) {
-        num_str[i++] = '0';
-    } else {
-        while (temp > 0) {
-            num_str[i++] = '0' + (temp % 10);
-            temp /= 10;
-        }
-    }
-    
-    // Reverse the string
-    for (int j = 0; j < i / 2; j++) {
-        char temp_char = num_str[j];
-        num_str[j] = num_str[i - 1 - j];
-        num_str[i - 1 - j] = temp_char;
-    }
-    num_str[i] = '\0';
-    
-    vga_writestring(num_str);
-    vga_putchar('\n');
-}
-
 void process_command(const char *command) {
     if (strlen(command) == 0) {
         return;
@@ -153,8 +107,6 @@ void process_command(const char *command) {
         vga_writestring("NumOS Version 1.0\n");
         vga_writestring("64-bit Operating System\n");
         vga_writestring("Built with C and Assembly!\n");
-    } else if (strcmp(command, "gdt") == 0) {
-        show_gdt_info();
     } else if (strncmp(command, "echo ", 5) == 0) {
         vga_writestring(command + 5);
         vga_putchar('\n');
