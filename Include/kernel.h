c529d1e5a4f1feb1856932103035948f7d1cb1b8
Luis <privatenumslower@gmail.com>
2025-08-14 06:31:53 -0500

    Updating NumOS files after recent edits

--- Include/kernel.h
+++ Include/kernel.h
@@ -32,6 +32,8 @@ char* kstrdup(const char* str);
 int strcmp(const char *str1, const char *str2);
 int strncmp(const char *str1, const char *str2, size_t n);
 char *strcpy(char *dest, const char *src);
+char *strcat(char *dest, const char *src);
+char *strstr(const char *haystack, const char *needle);
 
 /* System functions */
 void panic(const char *message);
